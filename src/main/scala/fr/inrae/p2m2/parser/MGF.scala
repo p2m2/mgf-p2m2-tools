9d4205d3d8cab10d8e84733d6b48af00b63158a7
Olivier Filangi <olivier.filangi@inrae.fr>
2023-07-17 17:04:22 +0200

    save modif

--- src/main/scala/fr/inrae/p2m2/parser/MGF.scala
+++ src/main/scala/fr/inrae/p2m2/parser/MGF.scala
@@ -1,6 +1,7 @@
 package fr.inrae.p2m2.parser
 
 import fr.inrae.p2m2.format.MGFFeaturesIon
+import fr.inrae.p2m2.parser.MGF.FeaturesBufferWithCurrentKey
 
 import scala.collection.Iterator
 
@@ -14,6 +15,23 @@ case object MGF {
       } map {
       case (_,idx) => idx // get All idx line
     }
+    Seq()
+  }
+  private type FeaturesBufferWithCurrentKey = (Option[String], Map[String, Map[String, String]])
+  def parse2(lines: Iterator[String]): Seq[MGFFeaturesIon] = {
+    val initParsing : FeaturesBufferWithCurrentKey = (None, Map())
+
+    lines.
+      foldLeft ( initParsing ) (
+        (currentKeyAndFeatures : FeaturesBufferWithCurrentKey, line : String)  => {
+
+        val r = { currentKeyAndFeatures match {
+              case (None,l) if line.contains("FEATURE_ID=") => (Some(line.split("=")(1)),l)
+              case _ =>  currentKeyAndFeatures
+            }
+          }
+          r
+        })
     Seq()
   }
 }
