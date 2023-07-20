c8b1f788550ed84dfe82a3d3e426c2294c3491c4
Olivier Filangi <olivier.filangi@inrae.fr>
2023-07-20 06:05:09 +0200

    save version

---
+++ src/main/scala/fr/inrae/p2m2/webapp/GeneralStatisticsHtmlDivManagement.scala
@@ -0,0 +1,85 @@
+package fr.inrae.p2m2.webapp
+
+import fr.inrae.p2m2.format.MGFFeaturesIon
+import fr.inrae.p2m2.visitor.PropertyIon
+import org.scalajs.dom
+import org.scalajs.dom.html.Div
+
+import scala.scalajs.js.Math.sqrt
+
+case object GeneralStatisticsHtmlDivManagement {
+  def setGeneralStatistics(listFeatures: Seq[MGFFeaturesIon]): Unit = {
+    dom
+      .document
+      .getElementById("numberFeatures").asInstanceOf[Div].innerHTML = listFeatures.length.toString
+
+    dom
+      .document
+      .getElementById("minPepmass").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.pepMass).min.toString
+
+    dom
+      .document
+      .getElementById("maxPepmass").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.pepMass).max.toString
+
+    dom
+      .document
+      .getElementById("minRt").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.retentionTime).min.toString
+
+    dom
+      .document
+      .getElementById("maxRt").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.retentionTime).max.toString
+
+    dom
+      .document
+      .getElementById("nbScans").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.scans).distinct.length.toString
+
+    dom
+      .document
+      .getElementById("msLevelList").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.msLevel).distinct.mkString(",")
+
+    dom
+      .document
+      .getElementById("chargeList").asInstanceOf[Div].innerHTML =
+      listFeatures.flatMap(PropertyIon.charge).distinct.mkString(",")
+
+    val l = listFeatures.map(_.fragmentIons.length)
+
+    val mean = (l.sum.toDouble / (l.length + 1))
+    val std = sqrt(l.map(x => (x - mean) * (x - mean)).sum / (l.length + 1))
+
+    dom
+      .document
+      .getElementById("meanFragIon").asInstanceOf[Div].innerHTML = mean.toString
+    dom
+      .document
+      .getElementById("stdFragIon").asInstanceOf[Div].innerHTML = std.toString
+
+    dom
+      .document
+      .getElementById("minMzFragIon").asInstanceOf[Div].innerHTML =
+      listFeatures.map(_.fragmentIons.map(_._1).min).min.toString
+
+    dom
+      .document
+      .getElementById("maxMzFragIon").asInstanceOf[Div].innerHTML =
+      listFeatures.map(_.fragmentIons.map(_._1).max).max.toString
+
+    dom
+      .document
+      .getElementById("minAbundanceFragIon").asInstanceOf[Div].innerHTML =
+      listFeatures.map(_.fragmentIons.map(_._2).min).min.toString
+
+    dom
+      .document
+      .getElementById("maxAbundanceFragIon").asInstanceOf[Div].innerHTML =
+      listFeatures.map(_.fragmentIons.map(_._2).max).max.toString
+
+  }
+
+}
--- src/main/scala/fr/inrae/p2m2/webapp/MGFWebApp.scala
+++ src/main/scala/fr/inrae/p2m2/webapp/MGFWebApp.scala
@@ -2,21 +2,20 @@ package fr.inrae.p2m2.webapp
 
 import fr.inrae.p2m2.format.MGFFeaturesIon
 import fr.inrae.p2m2.parser.MGF
-import fr.inrae.p2m2.visitor.{CaptureIonFragmentSource, PropertyIon}
+import fr.inrae.p2m2.webapp.GeneralStatisticsHtmlDivManagement.setGeneralStatistics
 import org.scalajs.dom
+import org.scalajs.dom.html.{Input, Progress}
 import org.scalajs.dom.{FileReader, HTMLInputElement}
-import org.scalajs.dom.html.{Div, Input, Progress}
 import scalatags.JsDom
 import scalatags.JsDom.all._
 
+import scala.annotation.unused
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.scalajs.js.Math.sqrt
-import scala.scalajs.js.URIUtils.encodeURIComponent
 import scala.util.{Failure, Success}
 
 object MGFWebApp {
-  def readFileAsText (file : dom.File) (implicit ec: ExecutionContext) : Future[String] = {
+  def readFileAsText (file : dom.File) (implicit @unused ec: ExecutionContext) : Future[String] = {
     val p = Promise[String]()
     val fr = new FileReader()
 
@@ -33,79 +32,6 @@ object MGFWebApp {
     p.future
   }
 
-  def setGeneralStatistics(listFeatures: Seq[MGFFeaturesIon]) : Unit = {
-    dom
-      .document
-      .getElementById("numberFeatures").asInstanceOf[Div].innerHTML = listFeatures.length.toString
-
-    dom
-      .document
-      .getElementById("minPepmass").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.pepMass).min.toString
-
-    dom
-      .document
-      .getElementById("maxPepmass").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.pepMass).max.toString
-
-    dom
-      .document
-      .getElementById("minRt").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.retentionTime).min.toString
-
-    dom
-      .document
-      .getElementById("maxRt").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.retentionTime).max.toString
-
-    dom
-      .document
-      .getElementById("nbScans").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.scans).distinct.length.toString
-
-    dom
-      .document
-      .getElementById("msLevelList").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.msLevel).distinct.mkString(",")
-
-    dom
-      .document
-      .getElementById("chargeList").asInstanceOf[Div].innerHTML =
-      listFeatures.flatMap(PropertyIon.charge).distinct.mkString(",")
-
-    val l = listFeatures.map(_.fragmentIons.length)
-
-    val mean = (l.sum.toDouble / (l.length+1))
-    val std = sqrt(l.map( x => (x - mean) * (x - mean) ).sum / (l.length+1))
-
-    dom
-      .document
-      .getElementById("meanFragIon").asInstanceOf[Div].innerHTML = mean.toString
-    dom
-      .document
-      .getElementById("stdFragIon").asInstanceOf[Div].innerHTML = std.toString
-
-    dom
-      .document
-      .getElementById("minMzFragIon").asInstanceOf[Div].innerHTML =
-      listFeatures.map(_.fragmentIons.map(_._1).min).min.toString
-
-    dom
-      .document
-      .getElementById("maxMzFragIon").asInstanceOf[Div].innerHTML =
-      listFeatures.map(_.fragmentIons.map(_._1).max).max.toString
-
-    dom
-      .document
-      .getElementById("minAbundanceFragIon").asInstanceOf[Div].innerHTML =
-      listFeatures.map(_.fragmentIons.map(_._2).min).min.toString
-
-    dom
-      .document
-      .getElementById("maxAbundanceFragIon").asInstanceOf[Div].innerHTML =
-      listFeatures.map(_.fragmentIons.map(_._2).max).max.toString
-
-  }
 
   def main(args: Array[String]): Unit = {
 
@@ -143,7 +69,9 @@ object MGFWebApp {
                           .document
                           .getElementById("file").asInstanceOf[Progress].setAttribute("value", percent.toString)
                     })
+
                     setGeneralStatistics(listFeatures)
+
                     /*
                     listFeatures.flatMap {
                       feature =>
