7dba7dbe1356e9e0d6c5595e5c1714c07e996682
Olivier Filangi <you@example.com>
2023-07-21 13:02:28 +0200

    stop develp stream.

--- src/main/scala/fr/inrae/p2m2/webapp/MGFWebApp.scala
+++ src/main/scala/fr/inrae/p2m2/webapp/MGFWebApp.scala
@@ -4,29 +4,36 @@ import fr.inrae.p2m2.format.MGFFeaturesIon
 import fr.inrae.p2m2.parser.MGF
 import fr.inrae.p2m2.webapp.GeneralStatisticsHtmlDivManagement.setGeneralStatistics
 import org.scalajs.dom
-import org.scalajs.dom.html.{Input, Progress}
-import org.scalajs.dom.{FileReader, HTMLInputElement}
+import org.scalajs.dom.html.{Div, Input, Progress}
+import org.scalajs.dom.{Event, FileReader, HTMLInputElement}
+import org.scalajs.dom.ProgressEvent
 import scalatags.JsDom
 import scalatags.JsDom.all._
 
 import scala.annotation.unused
 import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
+
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
 
 object MGFWebApp {
   def readFileAsText (file : dom.File) (implicit @unused ec: ExecutionContext) : Future[String] = {
+    P2M2Js.waitBlock(true)
     val p = Promise[String]()
     val fr = new FileReader()
 
     fr.onload = _ => {
       p.success(fr.result.toString)
-
     }
 
     fr.onerror = _ => {
+      System.err.println(s"** Can not parse file ** ")
       p.failure(new Exception())
     }
+
+    fr.onloadstart = _ => P2M2Js.waitBlock(true)
+
+    fr.onloadend = _ => P2M2Js.waitBlock(false)
 
     fr.readAsText(file,"ISO-8859-1")
     p.future
